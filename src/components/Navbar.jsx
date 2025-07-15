ba3678bc6dc0fbae903dd4f2a30433696ab0a02f
Sajitha-tech <sajithats1234@gmail.com>
2025-07-15 23:19:22 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+<nav class="navbar navbar-expand-lg bg-secondary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Grocery Cart</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Add Vehicle</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Search Vehicle</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Delete Vehicle</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">View All</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/Viewall.jsx
@@ -0,0 +1,120 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Viewall = () => {
+  return (
+    <div>
+<Navbar/>
+ <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                <div class="row g-5">
+                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                        <div class="card">
+  <img src="https://img.autocarindia.com/model/uploads/modelimages/Hyundai-Creta-030420250118.jpg?w=350&h=250&q=85&c=1" class="card-img-top" alt="...">
+  </img>
+  <div class="card-body">
+    <h5 class="card-title">Hyundai-Creta</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Add To Cart</a>
+  </div>
+</div>
+
+                    </div>
+                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+ <div class="card">
+  
+  <img src="https://img.autocarindia.com/model/uploads/modelimages/Hyundai-Creta-030420250118.jpg?w=350&h=250&q=85&c=1" class="card-img-top" alt="...">
+  </img>
+  <div class="card-body">
+    <h5 class="card-title">Hyundai-Creta</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Add To Cart</a>
+  </div>
+</div>
+
+                    </div>
+                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                         <div class="card">
+  <img src="https://img.autocarindia.com/model/uploads/modelimages/Hyundai-Creta-030420250118.jpg?w=350&h=250&q=85&c=1" class="card-img-top" alt="...">
+  </img>
+  <div class="card-body">
+    <h5 class="card-title">Hyundai-Creta</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Add To Cart</a>
+  </div>
+</div>
+                    </div>
+                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                         <div class="card">
+  <img src="https://img.autocarindia.com/model/uploads/modelimages/Hyundai-Creta-030420250118.jpg?w=350&h=250&q=85&c=1" class="card-img-top" alt="...">
+ </img>
+  <div class="card-body">
+    <h5 class="card-title">Hyundai-Creta</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Add To Cart</a>
+  </div>
+</div>
+                    </div>
+                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                         <div class="card">
+  <img src="https://img.autocarindia.com/model/uploads/modelimages/Hyundai-Creta-030420250118.jpg?w=350&h=250&q=85&c=1" class="card-img-top" alt="...">
+  </img>
+  <div class="card-body">
+    <h5 class="card-title">Hyundai-Creta</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Add To Cart</a>
+  </div>
+</div>
+                    </div>
+                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                         <div class="card">
+  <img src="https://img.autocarindia.com/model/uploads/modelimages/Hyundai-Creta-030420250118.jpg?w=350&h=250&q=85&c=1" class="card-img-top" alt="...">
+  </img>
+  <div class="card-body">
+    <h5 class="card-title">Hyundai-Creta</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Add To Cart</a>
+  </div>
+</div>
+                    </div>
+                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                         <div class="card">
+  <img src="https://img.autocarindia.com/model/uploads/modelimages/Hyundai-Creta-030420250118.jpg?w=350&h=250&q=85&c=1" class="card-img-top" alt="...">
+  </img>
+  <div class="card-body">
+    <h5 class="card-title">Hyundai-Creta</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Add To Cart</a>
+  </div>
+</div>
+                    </div>
+                    <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                         <div class="card">
+  <img src="https://img.autocarindia.com/model/uploads/modelimages/Hyundai-Creta-030420250118.jpg?w=350&h=250&q=85&c=1" class="card-img-top" alt="...">
+  </img>
+  <div class="card-body">
+    <h5 class="card-title">Hyundai-Creta</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Add To Cart</a>
+  </div>
+</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default Viewall
