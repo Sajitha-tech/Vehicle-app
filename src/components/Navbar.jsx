a68ff80fef7234d42a255ba4ad6f08ce9bb3f6c3
Sajitha-tech <sajithats1234@gmail.com>
2025-07-15 23:24:57 +0530

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div>
 <nav class="navbar navbar-expand-lg bg-secondary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Grocery Cart</a>
+    <a class="navbar-brand" href="#">VEHICLE</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
